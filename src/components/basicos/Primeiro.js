ebb10c1ad129031dabc75f93b5617ea95487968c
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-09-05 14:48:17 -0300

    primeiro componente

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,4 +1,11 @@
+// import React from 'react'
 
-export default function Primeiro(){
-    return 'Primeiro Componente'
+export default function Primeiro() {
+    const msg = 'Seja bem vinda!'
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p>
+        </div>
+    )
 }
