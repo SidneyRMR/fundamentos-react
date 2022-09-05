e5242a8622e2cb69e1ad1c889ba30ace0401b42b
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-09-05 20:45:42 -0300

    Gerando número aleatório

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from './components/basicos/Aleatorio'
 
-export default function App(props) {
-    return (
-        <div id='app'>
-        <Primeiro></Primeiro>
-        <ComParametro 
+export default () =>
+    <div id='app'>
+        <h1>Fundamentos React</h1>
+        <Aleatorio 
+            min = {10}
+            max = {50}/>
+        <Fragmento />
+        <ComParametro
             titulo="Situação do aluno"
             aluno="Pedro"
-            nota={5.50}/>
-        <ComParametro 
+            nota={5.50} />
+        <ComParametro
             titulo="Situação do aluno"
             aluno="Maria"
-            nota={9.9}/>
-            
+            nota={9.9} />
+        <Primeiro></Primeiro>
     </div>
-    )
-}
+
--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import './index.css'
 import { createRoot } from 'react-dom/client'
-import React from 'react'
-
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+import App from './App'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
+
 root.render(
-    <div id='app'>
-        <Primeiro></Primeiro>
-        <ComParametro 
-            titulo="Situação do aluno"
-            aluno="Pedro"
-            nota={5.50}/>
-        <ComParametro 
-            titulo="Situação do aluno"
-            aluno="Maria"
-            nota={9.9}/>
-        <Fragmento/>
-    </div>
+    <App/>
 )
